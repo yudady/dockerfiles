ac6c77bc77a901194be50fd93b731770eabde1ec
yudady <yudady@gmail.com>
2022-12-01 17:39:16 +0800

    add skaffold

--- skaffold/blog/client/src/PostList.jsx
+++ skaffold/blog/client/src/PostList.jsx
@@ -7,7 +7,7 @@ const PostList = () => {
     const [posts, setPosts] = useState({});
 
     const fetchPosts = async () => {
-        const result = await axios.get("http://posts.com/posts");
+        const result = await axios.get("http://localhost:4002/posts");
         setPosts(result.data);
     }
 
