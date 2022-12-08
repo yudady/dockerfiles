d2361a2735cdf5288d7b2eb417300b9100d684da
yudady <yudady@gmail.com>
2022-12-08 09:57:45 +0800

    update go http mygo

--- pack-cli/golang/http/main.go
+++ pack-cli/golang/http/main.go
@@ -4,22 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
-const INDEX = `<!DOCTYPE html>
-<html>
-  <head>
-    <title>myapp</title>
-  </head>
-  <body>
-    <h1>myapp:go:<span style="color:red">v3</span></h1>
-  </body>
-</html>`
+func html(hostname string) string {
+	return `<!DOCTYPE html>
+	<html>
+	  <head>
+		<title>myapp</title>
+	  </head>
+	  <body>
+		<h1>myapp:go:<span style="color:red">v3</span> hostname :　` + hostname + `</h1>
+	  </body>
+	</html>`
+}
 
 func main() {
+	hostname, _ := os.Hostname()
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprint(w, INDEX)
+		fmt.Fprint(w, html(hostname))
 	})
-
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
